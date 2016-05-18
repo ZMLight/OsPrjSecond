3e0c99cef00a457687e8864cd5e766525598aa0b
ZMLight <shzhao2015@gmail.com>
2016-05-18 17:19:06 +0800

    init version

---
+++ goldfish/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Tue May 17 13:03:20 CST 2016"
+#define LINUX_COMPILE_BY "zhaoming"
+#define LINUX_COMPILE_HOST "Light"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
