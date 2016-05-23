21d970602f575ffd6aa95fd1d1662729aefd0e74
ZMLight <shzhao2015@gmail.com>
2016-05-23 14:37:41 +0800

    get back

--- goldfish/include/generated/compile.h
+++ goldfish/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Wed May 18 21:00:16 CST 2016"
+#define UTS_VERSION "#9 PREEMPT Mon May 23 14:35:46 CST 2016"
 #define LINUX_COMPILE_BY "zhaoming"
 #define LINUX_COMPILE_HOST "Light"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
